6b86aff67dfa51b3466374ff279b98ae47c71bdb
Ali Obeidat <90185197+Ali-Obeidat@users.noreply.github.com>
2021-10-28 11:51:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector(".theem-btn");
+let body = document.querySelector("body");
+btn.addEventListener("click", function () {
+  body.style.background = "#20325a";
+  body.style.color = "white";
+});
